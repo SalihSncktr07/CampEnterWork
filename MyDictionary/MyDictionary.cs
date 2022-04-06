eaad564447d7539f2335b47fc2ae3d82be400816
Ben Benim <Ben_Benim@outlook.com.tr>
2022-04-07 01:26:09 +0300

    4.Gün, 5.Ödev MyDictionary

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionaryDemo
+{
+    public class MyDictionary<TKey,TValue>
+    {
+        TKey[] _keys;
+        TValue[] _values;
+
+        TKey[] _tempKeys;
+        TValue[] _tempValues;
+
+        public MyDictionary()
+        {
+            _keys = new TKey[0];
+            _values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            _tempKeys = _keys;
+            _tempValues = _values;
+            _keys = new TKey[_keys.Length + 1];
+            _values = new TValue[_values.Length + 1];
+
+            for (int i = 0; i < _tempKeys.Length; i++)
+            {
+                _keys[i] = _tempKeys[i];
+                _values[i] = _tempValues[i];
+            }
+            _keys[_keys.Length - 1] = key;
+            _values[_keys.Length - 1] = value;
+        }
+
+        public TValue GetValueOrDefault(TKey key)
+        {
+            for (int i = 0; i < _keys.Length; i++)
+            {
+               if(_keys[i].ToString() == key.ToString())
+                {
+                    return _values[i];
+                }
+            }
+
+            throw new Exception("Anahtara karşılık gelen bir değer bulunamadı");
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using MyDictionaryDemo;
+using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string,int> days= new MyDictionary<string,int>();
+
+            days.Add("Pazartesi",1);
+            days.Add("Salı",2);
+            days.Add("Çarşamba",3);
+            days.Add("Perşembe",4);
+            days.Add("Cuma",5);
+            days.Add("Cumartesi",6);
+            days.Add("Pazar",7);
+            Console.WriteLine(days.GetValueOrDefault("Cumartesisw"));
+        }
+    }
+}
