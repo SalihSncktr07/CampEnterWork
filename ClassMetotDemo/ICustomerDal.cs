04bcce3cc6dfe6060ef9c0f28eff06ed4f7929dc
Ben Benim <Ben_Benim@outlook.com.tr>
2022-04-07 01:50:19 +0300

    3.Gün 3.Ödev

--- ClassMetotDemo/ICustomerDal.cs
+++ ClassMetotDemo/ICustomerDal.cs
@@ -12,33 +12,4 @@ namespace ClassMetotDemo
         void Delete(Customer customer);
         List<Customer> GetAll();
     }
-
-    public class CustomerDal : ICustomerDal
-    {
-        List<Customer> _customers;
-
-        public CustomerDal()
-        {
-            _customers = new List<Customer> {
-                new Customer {Id=1,Name="Mustafa",LastName="Aktoz" },
-                new Customer {Id=2,Name="Onur",LastName="Şimşek" },
-                new Customer {Id=3,Name="Mehmet",LastName="Demir" },
-            };
-        }
-
-        public void Add(Customer customer)
-        {
-            _customers.Add(customer);
-        }
-
-        public List<Customer> GetAll()
-        {
-            return _customers;
-        }
-
-        public void Delete(Customer customer)
-        {
-            _customers.Remove(customer);
-        }
-    }
 }
